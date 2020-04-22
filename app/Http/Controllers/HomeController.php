2126d6f943a5641aa1605d4baa5cf491149f79b3
unknown <nickarsenal007@gmail.com>
2020-04-22 12:28:05 +0545

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \Pusher\Pusher;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function authenticate(Request $request) {
+        $socketId = $request->socket_id;
+        $channelName = $request->channel_name;
+
+        $pusher = new Pusher('60c8b5ef0d5fd3b4deb8', '9e292ca96a7804845936', '986230', [
+           'cluster' => 'ap2',
+           'encrypted' => true 
+        ]);
+
+        $presence_data = ['name' => auth()->user()->name];
+        $key = $pusher->presence_auth($channelName, $socketId, auth()->id(), $presence_data);
+
+        return response($key);
+
+    }
+}
---
+++ resources/assets/js/components/App.js
@@ -0,0 +1,145 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import MediaHandler from '../MediaHandler';
+import Pusher from 'pusher-js';
+import Peer from 'simple-peer';
+
+const APP_KEY = '60c8b5ef0d5fd3b4deb8';
+
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+		let ids = JSON.parse(this.props.data);
+		this.state = {
+			hasMedia: false,
+			otherUserId: null,
+			alluserids: ids,
+		};
+
+		this.user = window.user;
+		this.user.stream = null;
+		this.peers = {};
+
+		this.mediaHandler = new MediaHandler();
+		this.setupPusher();
+
+		this.callTo = this.callTo.bind(this);
+		this.setupPusher = this.setupPusher.bind(this);
+		this.startPeer = this.startPeer.bind(this);
+	}
+
+	componentWillMount() {
+		this.mediaHandler.getPermissions().then((stream) => {
+			this.setState({ hasMedia: true });
+			this.user.stream = stream;
+
+			try {
+				this.myVideo.srcObject = stream;
+			} catch (e) {
+				this.myVideo.src = URL.createObjectURL(stream);
+			}
+
+			this.myVideo.play();
+		})
+	}
+
+	setupPusher() {
+		this.pusher = new Pusher(APP_KEY, {
+			authEndpoint: '/pusher/auth',
+			cluster: 'ap2',
+			auth: {
+				params: this.user.id,
+				headers: {
+					'X-CSRF-Token': window.csrfToken
+				}
+			}
+		});
+
+		this.channel = this.pusher.subscribe('presence-video-channel');
+
+		this.channel.bind(`client-signal-${this.user.id}`, (signal) => {
+			let peer = this.peers[signal.userId];
+
+			if (peer === undefined) {
+				this.setState({ otherUserId: signal.userId });
+				peer = this.startPeer(signal.userId, false);
+			}
+
+			peer.signal(signal.data);
+		});
+	}
+
+	startPeer(userId, initiator = true) {
+		const peer = new Peer({
+			initiator,
+			stream: this.user.stream,
+			trickle: false
+		});
+
+		peer.on('signal', (data) => {
+			this.channel.trigger(`client-signal-${userId}`, {
+				type: 'signal',
+				userId: this.user.id,
+				data: data
+			});
+		});
+
+		peer.on('stream', (stream) => {
+			try {
+				this.userVideo.srcObject = stream;
+			} catch (e) {
+				this.userVideo.src = URL.createObjectURL(stream);
+			}
+
+			this.userVideo.play();
+		});
+
+		peer.on('close', () => {
+			let peer = this.peers[userId];
+			if ( peer !== undefined ) {
+				peer.destroy();
+			}
+			this.peers[userId] = undefined;
+		});
+
+		return peer;
+	} 
+
+	callTo(userId) {
+		this.peers[userId] = this.startPeer(userId);
+	}
+
+	render() {
+		return (
+			<div className="App container">
+                <div className="row justify-content-center">
+                    <div className="col-md-12">
+                        <div className="card">
+                            <div className="card-header">My Video</div>
+
+                            <div className="card-body">
+                                {
+									this.state.alluserids.map(
+										(singleuser) => {
+											return this.user.id !== singleuser.id ? <button key={singleuser.id} onClick={() => this.callTo(singleuser.id)}>Call {singleuser.name}</button> : null;
+										}
+									)
+								}
+
+                                <div className="video-container">
+                                    <video className="my-video" ref={ref => {this.myVideo = ref;}}/>
+                                    <video className="user-video" ref={ref => {this.userVideo = ref;}}/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+		);
+	}
+}
+
+if (document.getElementById('app')) {
+	var data = document.getElementById('userIds').getAttribute('data');
+	ReactDOM.render(<App data={data} />, document.getElementById('app'));
+}
